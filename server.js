c0f0ad152b3d5b9e582e6751828c8ba622d6dd38
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-10-14 10:46:31 +0300

    Define routes using express router

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const connectDB = require('./config/db');
 const app = express();
 //Connect database
 connectDB();
-
-//Endpoints
+//Connect app
 app.get('/', (req, res) => {
     res.send(`API is running.`);
 });
+
+//Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profiles', require('./routes/api/profiles'));
+
 
 const PORT = process.env.PORT || 7000;
 
