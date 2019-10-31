dc6dffca55599da056e4da2e12d89bc7c4e44a56
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-10-31 15:32:27 +0200

    Login user route using bcrypt and send back token if success

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 //middleware
 const auth = require('../../middleware/auth');
 
 //models
 const User = require('../../models/User');
 
+
 //@route GET api/auth
 //@desc Get authorized user's info
-//@access Public
+//@access Private
 router.get('/', auth, async (req, res) => {
     try {
         //Search in the db for the user with same id, output the user from db, leaving out the password
@@ -17,6 +22,60 @@ router.get('/', auth, async (req, res) => {
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server Error.');
+    }
+});
+
+//@route POST api/users
+//@desc Log in user
+//@access Public
+router.post('/', [
+    check('email', 'Please enter a valid email.').isEmail(),
+    check('password', 'Password is required.').exists(), //Only check if the user input the password
+], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    } else {
+        const { email, password } = req.body;
+
+        try {
+            //See if user exists
+            let userInDB = await User.findOne({ email: email });
+
+            if (!userInDB) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials.' }] }); //show no specific errors to prevent security holes
+            } else {
+                //check if the password matches
+                const isMatch = await bcrypt.compare(password, userInDB.password);
+
+                if (!isMatch) {
+                    return res.status(400).json({ errors: [{ msg: 'Invalid Credentials.' }] }); //same explanation above
+                }
+
+                let payload = {
+                    user: {
+                        id: userInDB.id,
+                        name: userInDB.name,
+                        email: userInDB.email,
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtKey'),
+                    { expiresIn: 360000 },
+                    (error, token) => {
+                        if (error) throw error;
+
+                        res.json({ token });
+                    }
+                );
+            }
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server error.');
+        }
     }
 });
 
