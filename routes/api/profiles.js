0f1db53cd2ed898cec8060e96d11a8c919294dce
chuongcuu <dangchuong1999@outlook.com>
2019-11-01 13:08:53 +0200

    Create Profile model and route for getting current user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const {
+    check,
+    validationResult
+} = require('express-validator');
+//models
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
-//@route GET api/profiles
-//@desc Test route
-//@access Public
-router.get('/', (req, res) => {
-    res.send('Profiles route');
+//middleware
+const auth = require('../../middleware/auth');
+
+//@route GET api/profiles/me
+//@desc Get current user profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user.'
+            });
+        }
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error.');
+    }
 });
+
+//@route POST api/profiles
+//@desc Create or Update a user's profile
+//@access Private
+router.post('/', [
+    auth,
+    check('status', 'Status must be provided.').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+});
+
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -15,21 +18,35 @@ const User = require('../../models/User');
 router.post('/', [
     check('name', 'Name is required.').not().isEmpty(),
     check('email', 'Please enter a valid email.').isEmail(),
-    check('password', 'Please enter a password with min length of 6 characters.').isLength({ min: 6 }),
+    check('password', 'Please enter a password with min length of 6 characters.').isLength({
+        min: 6
+    }),
 ], async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({
+            errors: errors.array()
+        });
     } else {
-        const { name, email, password } = req.body;
+        const {
+            name,
+            email,
+            password
+        } = req.body;
 
         try {
             //See if user exist
-            let user = await User.findOne({ email: email });
+            let user = await User.findOne({
+                email: email
+            });
 
             if (user) {
-                return res.status(400).json({ errors: [{ msg: 'User already exists.' }] });
+                return res.status(400).json({
+                    errors: [{
+                        msg: 'User already exists.'
+                    }]
+                });
             } else {
                 //Get user avatar
                 let avatar = gravatar.url(email, {
@@ -81,12 +98,15 @@ router.post('/', [
 
                 jwt.sign(
                     payload,
-                    config.get('jwtKey'),
-                    { expiresIn: 360000 },
+                    config.get('jwtKey'), {
+                        expiresIn: 360000
+                    },
                     (error, token) => {
                         if (error) throw error;
 
-                        res.json({ token });
+                        res.json({
+                            token
+                        });
                     }
                 );
             }
@@ -95,6 +115,7 @@ router.post('/', [
             res.status(500).send('Server error.');
         }
     }
-})
+});
+
 
 module.exports = router;
