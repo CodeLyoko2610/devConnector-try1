c572f0529827156044278a88d76a272273837bf7
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-11-13 11:25:23 +0200

    Route for deleting education from user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -307,6 +307,28 @@ router.put('/education', [auth, [
     console.error(error.message);
     res.status(500).send('Server error.');
   }
-})
+});
+
+//@route DELETE api/profiles/education/:edu_id
+//@desc Delete an education from 1 user's profile
+//@access Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1); //remove x items from the specified index
+
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error.');
+  }
+});
 
 module.exports = router;
