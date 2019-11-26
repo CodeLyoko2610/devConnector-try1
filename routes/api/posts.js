aa39473f45226d35f6822074ad962931f4c608cb
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-11-26 15:46:50 +0200

    Route for like a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -108,5 +108,26 @@ router.delete('/:post_id', auth, async (req, res) => {
     }
 });
 
+//@route PUT api/posts/like/:post_id
+//@desc Like a post
+//@access Private
+router.put('/like/:post_id', auth, async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.post_id);
+
+        //Check if post has been liked
+        if (post.likes.filter(like => (like.user.toString() === req.user.id)).length > 0) {
+            return res.status(400).json({ msg: 'Post has already been liked.' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+        await post.save();
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error.');
+    }
+});
 
 module.exports = router;
