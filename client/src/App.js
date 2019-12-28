981482ddcee2e51d49c5d0647be0651e92697501
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-12-28 23:37:20 +0200

    Using React Router to create route to Landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 //Import components
@@ -6,10 +7,12 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+    </Fragment>
+  </Router>
 );
 
 export default App;
