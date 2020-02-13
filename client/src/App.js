f415e62961da40f97545376b96d79e3bab451d8f
chuongcuu <dangchuong1999@outlook.com>
2020-02-13 13:36:17 +0200

    Add component Login and Register.

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,20 @@ import './App.css';
 //Import components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
-      <Route exact path="/" component={Landing} />
+      <Route exact path='/' component={Landing} />
+
+      {/* Div with a class of container to push pages in the center, instead of displaying from top of the screen like landing page.  */}
+      <section className='container'>
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+      </section>
     </Fragment>
   </Router>
 );
